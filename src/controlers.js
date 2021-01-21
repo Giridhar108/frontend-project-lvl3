5c1b44fddccb90fe0a17d19419a066aadd975d92
German Fomenko <pentagram777@mail.ru>
2021-01-21 16:12:26 +0500

    fix errors

--- src/controlers.js
+++ src/controlers.js
@@ -1,8 +1,8 @@
-import * as yup from "yup";
-import { setLocale } from "yup";
-import axios from "axios";
-import i18next from "i18next";
-import parsing from "./parsing";
+import * as yup from 'yup';
+import { setLocale } from 'yup';
+import axios from 'axios';
+import i18next from 'i18next';
+import parsing from './parsing';
 
 const pushAdded = (main, items, state) => {
   main.forEach((a) => {
@@ -15,42 +15,40 @@ const pushAdded = (main, items, state) => {
       state.added.push(a.pubDate);
     }
   });
-  state.status = "processed";
+  state.status = 'processed';
 };
 
 export const getData = (state) => {
-  state.url.map((url) =>
-    axios
-      .get(
-        `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(
-          url
-        )}`
-      )
-      // axios.get(`https://cors-anywhere.herokuapp.com/${url}`)
-      .then((response) => {
-        const feedData = parsing(response.data.contents);
-        if (feedData === "Error") {
-          state.status = "Wrong rss";
-          // throw new Error(`Wrong ${document}`);
-          return;
-        }
-        state.checkedUrl.push(response.config.url);
-        state.main.push(feedData.main);
-        state.items.push(feedData.items);
-        state.status = "processing";
-        pushAdded(state.main, state.items, state);
-      })
-      .catch((e) => {
-        console.log(e);
-        state.url = [];
-        state.status = "Network Error";
-      })
-  );
+  state.url.map((url) => axios
+    .get(
+      `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(
+        url,
+      )}`,
+    )
+  // axios.get(`https://cors-anywhere.herokuapp.com/${url}`)
+    .then((response) => {
+      const feedData = parsing(response.data.contents);
+      if (feedData === 'Error') {
+        state.status = 'Wrong rss';
+        // throw new Error(`Wrong ${document}`);
+        return;
+      }
+      state.checkedUrl.push(response.config.url);
+      state.main.push(feedData.main);
+      state.items.push(feedData.items);
+      state.status = 'processing';
+      pushAdded(state.main, state.items, state);
+    })
+    .catch((e) => {
+      console.log(e);
+      state.url = [];
+      state.status = 'Network Error';
+    }));
 };
 
 setLocale({
   number: {
-    min: ({ min }) => ({ key: i18next.t("setLocale"), values: { min } }),
+    min: ({ min }) => ({ key: i18next.t('setLocale'), values: { min } }),
   },
 });
 
@@ -75,25 +73,24 @@ const schema = yup.object().shape({
 export const getUrl = (e, state) => {
   e.preventDefault();
   const formData = new FormData(e.target);
-  const url = formData.get("url");
+  const url = formData.get('url');
   const validate = (url, state) => {
     try {
-      schema.validateSync({ url: url });
+      schema.validateSync({ url });
       return true;
     } catch (e) {
       return false;
     }
   };
 
-  validate(url, state);
   if (validate(url, state) && !state.url.includes(url)) {
-    state.status = "valid";
+    state.status = 'valid';
   } else if (validate(url, state) && state.url.includes(url)) {
-    state.status = "was";
+    state.status = 'was';
   } else {
-    state.status = "invalid";
+    state.status = 'invalid';
   }
-  if (!state.checkedUrl.includes(url) && state.status === "valid") {
+  if (!state.checkedUrl.includes(url) && state.status === 'valid') {
     state.url.push(url.trim());
     getData(state);
   }
@@ -110,20 +107,18 @@ export const listenModal = (event, state) => {
 };
 
 export const fetchNewPosts = (watchedState) => {
-  const promises = watchedState.url.map((url) =>
-    axios
-      .get(`https://cors-anywhere.herokuapp.com/${url}`)
-      .then((response) => {
-        const feedData = parsing(response.data);
-        watchedState.items.push(feedData.items);
-        watchedState.status = "processing";
-        pushAdded(watchedState.main, watchedState.items, watchedState);
-        console.log(feedData);
-      })
-      .catch((e) => {
-        console.log(e);
-      })
-  );
+  const promises = watchedState.url.map((url) => axios
+    .get(`https://cors-anywhere.herokuapp.com/${url}`)
+    .then((response) => {
+      const feedData = parsing(response.data);
+      watchedState.items.push(feedData.items);
+      watchedState.status = 'processing';
+      pushAdded(watchedState.main, watchedState.items, watchedState);
+      console.log(feedData);
+    })
+    .catch((e) => {
+      console.log(e);
+    }));
   Promise.all(promises).finally(() => {
     setTimeout(() => fetchNewPosts(watchedState), 5000);
   });

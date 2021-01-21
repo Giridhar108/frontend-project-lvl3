fc8780d84cd1a90fc3d2dd251eab7629f19112d7
German Fomenko <pentagram777@mail.ru>
2021-01-21 16:06:05 +0500

    feat sync validate

--- src/controlers.js
+++ src/controlers.js
@@ -1,8 +1,8 @@
-import * as yup from 'yup';
-import { setLocale } from 'yup';
-import axios from 'axios';
-import i18next from 'i18next';
-import parsing from './parsing';
+import * as yup from "yup";
+import { setLocale } from "yup";
+import axios from "axios";
+import i18next from "i18next";
+import parsing from "./parsing";
 
 const pushAdded = (main, items, state) => {
   main.forEach((a) => {
@@ -15,69 +15,88 @@ const pushAdded = (main, items, state) => {
       state.added.push(a.pubDate);
     }
   });
-  state.status = 'processed';
+  state.status = "processed";
 };
 
 export const getData = (state) => {
-  state.url.map((url) => axios
-    .get(`https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(url)}`)
-  // axios.get(`https://cors-anywhere.herokuapp.com/${url}`)
-    .then((response) => {
-      const feedData = parsing(response.data.contents);
-      if (feedData === 'Error') {
-        state.status = 'Wrong rss';
-        // throw new Error(`Wrong ${document}`);
-        return;
-      }
-      state.checkedUrl.push(response.config.url);
-      state.main.push(feedData.main);
-      state.items.push(feedData.items);
-      state.status = 'processing';
-      pushAdded(state.main, state.items, state);
-    })
-    .catch((e) => {
-      console.log(e);
-      state.url = [];
-      state.status = 'Network Error';
-    }));
+  state.url.map((url) =>
+    axios
+      .get(
+        `https://hexlet-allorigins.herokuapp.com/get?url=${encodeURIComponent(
+          url
+        )}`
+      )
+      // axios.get(`https://cors-anywhere.herokuapp.com/${url}`)
+      .then((response) => {
+        const feedData = parsing(response.data.contents);
+        if (feedData === "Error") {
+          state.status = "Wrong rss";
+          // throw new Error(`Wrong ${document}`);
+          return;
+        }
+        state.checkedUrl.push(response.config.url);
+        state.main.push(feedData.main);
+        state.items.push(feedData.items);
+        state.status = "processing";
+        pushAdded(state.main, state.items, state);
+      })
+      .catch((e) => {
+        console.log(e);
+        state.url = [];
+        state.status = "Network Error";
+      })
+  );
 };
 
-export const validate = (url, state) => {
-  setLocale({
-    number: {
-      min: ({ min }) => ({ key: i18next.t('setLocale'), values: { min } }),
-    },
-  });
+setLocale({
+  number: {
+    min: ({ min }) => ({ key: i18next.t("setLocale"), values: { min } }),
+  },
+});
 
-  const schema = yup.object().shape({
-    url: yup.string().min(5).url(),
-  });
+const schema = yup.object().shape({
+  url: yup.string().min(5).url(),
+});
 
-  return schema
-    .isValid({
-      url: `${url}`,
-    })
-    .then((valid) => {
-      if (valid && !state.url.includes(url)) {
-        state.status = 'valid';
-      } else if (valid && state.url.includes(url)) {
-        state.status = 'was';
-      } else {
-        state.status = 'invalid';
-      }
-    });
-};
+// return schema
+//   .isValid({
+//     url: `${url}`,
+//   })
+//   .then((valid) => {
+//     if (valid && !state.url.includes(url)) {
+//       state.status = 'valid';
+//     } else if (valid && state.url.includes(url)) {
+//       state.status = 'was';
+//     } else {
+//       state.status = 'invalid';
+//     }
+//   });
 
 export const getUrl = (e, state) => {
   e.preventDefault();
   const formData = new FormData(e.target);
-  const url = formData.get('url');
-  validate(url, state).then(() => {
-    if (!state.checkedUrl.includes(url) && state.status === 'valid') {
-      state.url.push(url.trim());
-      getData(state);
+  const url = formData.get("url");
+  const validate = (url, state) => {
+    try {
+      schema.validateSync({ url: url });
+      return true;
+    } catch (e) {
+      return false;
     }
-  });
+  };
+
+  validate(url, state);
+  if (validate(url, state) && !state.url.includes(url)) {
+    state.status = "valid";
+  } else if (validate(url, state) && state.url.includes(url)) {
+    state.status = "was";
+  } else {
+    state.status = "invalid";
+  }
+  if (!state.checkedUrl.includes(url) && state.status === "valid") {
+    state.url.push(url.trim());
+    getData(state);
+  }
 };
 
 export const openModal = (event, state) => {
@@ -91,18 +110,20 @@ export const listenModal = (event, state) => {
 };
 
 export const fetchNewPosts = (watchedState) => {
-  const promises = watchedState.url.map((url) => axios
-    .get(`https://cors-anywhere.herokuapp.com/${url}`)
-    .then((response) => {
-      const feedData = parsing(response.data);
-      watchedState.items.push(feedData.items);
-      watchedState.status = 'processing';
-      pushAdded(watchedState.main, watchedState.items, watchedState);
-      console.log(feedData);
-    })
-    .catch((e) => {
-      console.log(e);
-    }));
+  const promises = watchedState.url.map((url) =>
+    axios
+      .get(`https://cors-anywhere.herokuapp.com/${url}`)
+      .then((response) => {
+        const feedData = parsing(response.data);
+        watchedState.items.push(feedData.items);
+        watchedState.status = "processing";
+        pushAdded(watchedState.main, watchedState.items, watchedState);
+        console.log(feedData);
+      })
+      .catch((e) => {
+        console.log(e);
+      })
+  );
   Promise.all(promises).finally(() => {
     setTimeout(() => fetchNewPosts(watchedState), 5000);
   });
